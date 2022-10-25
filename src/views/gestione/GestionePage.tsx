ab3ea69493e970a4e7ae01f294bfc9413fddba89
StefanoDeDonatis <dedonatisstefano02@gmail.com>
2022-10-25 12:47:39 +0200

    GESTIONE completamento tabs

--- src/views/gestione/GestionePage.tsx
+++ src/views/gestione/GestionePage.tsx
@@ -1,26 +1,19 @@
 import { Tab, Tabs } from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 import Convenzioni from "./Convenzioni";
-import Dati from "./Dati";
 import EliminaConto from "./EliminaConto";
 
 const GestionePage = () => {
     return (
         <Card className="w-75 mx-auto my-5" bg="light">
             <Tabs
-                defaultActiveKey="ilMioCOnto"
+                defaultActiveKey="costi"
                 id="fill-tab-example"
                 className="mb-3"
                 justify
             >
-                <Tab eventKey="ilMioCOnto" title="IL MIO CONTO">
-                    <Dati />
-                </Tab>
                 <Tab eventKey="costi" title="COSTI">
                     <Convenzioni />
-                </Tab>
-                <Tab eventKey="datiPatrimoniali" title="DATI PATRIMONIALI">
-
                 </Tab>
                 <Tab eventKey="eliminaConto" title="ELIMINA CONTO">
                     <EliminaConto />
