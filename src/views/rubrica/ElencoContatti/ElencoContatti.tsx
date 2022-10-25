5b1c6833c248c35da4a979a68fe65c2ba3439d59
Francesco-Camerino <frcamerino@gmail.com>
2022-10-25 14:35:56 +0200

    WIP ElencoContatti

--- src/views/rubrica/ElencoContatti/ElencoContatti.tsx
+++ src/views/rubrica/ElencoContatti/ElencoContatti.tsx
@@ -1,12 +1,13 @@
 
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import RouteEnum from "../../../constants/RouteEnum";
 import { ContattoModel } from "../../../models/contatto/ContattoModel";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import { modificaContatto } from "../../../store/rubrica/actions";
 import { elencoContatti } from "../../../store/rubrica/selectors";
 
 function ElencoContatti() {
-    const showForm: boolean = false;
     const dispatch = useAppDispatch();
     const contatti: ContattoModel[] = useAppSelector(elencoContatti);
     const navigate = useNavigate();
@@ -14,21 +15,21 @@ function ElencoContatti() {
         navigate(RouteEnum.InserimentoContatto);
     };
 
-    // const onClickModificaContatto = (contattoDaTrovare: ContattoModel) => {
+    const onClickModificaContatto = (contattoDaTrovare: ContattoModel) => {
 
 
-    //     const contattoModificato: ContattoModel = {
-    //         idContatto: contattoDaModificare.idContatto,
-    //         denominazione: contattoDaModificare.denominazione,
-    //         iban: contattoDaModificare.iban
-    //     };
-    //     dispatch(modificaContatto(contattoModificato));
-    // };
+        const contattoModificato: ContattoModel = {
+            idContatto: contattoDaTrovare.idContatto,
+            denominazione: contattoDaTrovare.denominazione,
+            iban: contattoDaTrovare.iban
+        };
+        dispatch(modificaContatto(contattoModificato));
+    };
 
     return (
         <>
 
-            {/* <Container>
+            <Container>
                 <Row>
                     <Col>
 
@@ -70,46 +71,7 @@ function ElencoContatti() {
                     </Col>
                 </Row>
 
-                {showForm && (<Row>
-                    <Col>
-                        <Card className="w-50 mx-auto my-5" bg="light">
-                            <Card.Body>
-                                <Card.Title className="text-center">
-                                    <div className="clearfix"></div>
-                                    <h3 className="mb-3">Inserisci Contatto</h3>
-
-                                </Card.Title>
-                                <Card.Body>
-                                    <Row>
-                                        <Col>
-                                            <Form onSubmit={() => onClickModificaContatto()}>
-
-
-                                                <CampoInput controlId="formInserimentoDenominazione" label="Denominazione" type="text" placeholder="denominazione" value={denominazione} setValue={setDenominazione} />
-
-                                                <CampoInput controlId="formInserimentoIBAN" label="IBAN" type="text" placeholder="IBAN" value={iban} setValue={setIban} />
-
-                                                <Row>
-                                                    <Col className="text-center mt-3">
-                                                        <Button variant="primary" type="submit">
-                                                            Inserisci
-                                                        </Button>
-                                                    </Col>
-                                                    <Col className="text-center mt-3">
-                                                        <Button variant="secondary" type="submit">
-                                                            Annulla
-                                                        </Button>
-                                                    </Col>
-                                                </Row>
-                                            </Form>
-                                        </Col>
-                                    </Row>
-                                </Card.Body>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>)}
-            </Container> */}
+            </Container>
         </>
     );
 }
